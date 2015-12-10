555fac807f7c8c19dfc33c4bfbf6499d99208ea1
Nguyen Phu Hai <nguyenphuhai1710@ymail.com>
2015-12-10 15:28:22 +0700

    plot4

--- plot4.R
+++ plot4.R
@@ -7,9 +7,13 @@ plotData <- ds[(ds$Date >= '2007-02-01' & ds$Date <= '2007-02-02'),]
 datetime <- as.POSIXct(paste(plotData$Date, plotData$Time))
 
 # Plot data
-png('plot3.png', height=480, width=480)
+png('plot4.png', height=480, width=480)
+par(mfrow=c(2,2))
+plot(plotData$Global_active_power~datetime, type='l', xlab='', ylab='Global Active Power')
+plot(plotData$Voltage~datetime, type='l', xlab='datetime', ylab='Voltage')
 plot(plotData$Sub_metering_1~datetime, type='l', ylab='Energy sub metering', xlab='')
 lines(plotData$Sub_metering_2~datetime, type='l', col='red')
 lines(plotData$Sub_metering_3~datetime, type='l', col='blue')
 legend('topright', col=c('black', 'red', 'blue'), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=1, lwd=2)
+plot(plotData$Global_reactive_power~datetime, type='l', xlab='datetime', ylab='Global_reactive_power')
 dev.off()
